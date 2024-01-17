a77c94eb3cb47329d2c0989c2a253d5a6ac54ba2
Kisjolanka01 <lytwynd08@gmail.com>
2024-01-17 12:45:15 +0100

    lmao

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,9 @@
 //console.log("Szia Lajos!");
+function osszead(num1, num2){
+    document.body.children[1].innerHTML=num1+num2
+        return num1+num2;
+}
+
+var a=3;
+var b=5;
+var c=osszead(a,b);
